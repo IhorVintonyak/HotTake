16408d4ad6d56442ccdf6ca19a068f085f1a27e9
Ihor Vintonyak <vintonyakihor@gmail.com>
2025-08-04 10:27:25 +0200

    Implementation of working but unfinished swipe functions

--- app/src/main/java/com/example/hottake/MainActivity.java
+++ app/src/main/java/com/example/hottake/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.hottake;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mainWindowText;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        mainWindowText = findViewById(R.id.mainWindowText);
+
+        mainWindowText.setOnTouchListener(new OnSwipeTouchListener(this) {
+            @Override
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "Свайп вправо", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                Toast.makeText(MainActivity.this, "Свайп влево", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeTop() {
+                Toast.makeText(MainActivity.this, "Свайп вверх", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeBottom() {
+                Toast.makeText(MainActivity.this, "Свайп вниз", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
