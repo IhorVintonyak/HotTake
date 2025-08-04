c8e1ab0b391d4e466a20ab4f2dc8ecf6d86afefd
Ihor Vintonyak <vintonyakihor@gmail.com>
2025-08-04 11:32:51 +0200

    Visual presentation of swipe functions

--- app/src/main/java/com/example/hottake/MainActivity.java
+++ app/src/main/java/com/example/hottake/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hottake;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mainWindowText;
+    private TextView badNumber;
+    private TextView goodNumber;
+    private TextView skipNumber;
+    private TextView forReworkNumber;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -18,26 +23,40 @@ public class MainActivity extends AppCompatActivity {
 
         mainWindowText = findViewById(R.id.mainWindowText);
 
+        badNumber = findViewById( R.id.badNumber );
+        goodNumber = findViewById( R.id.goodNumber );
+        skipNumber = findViewById( R.id.skipNumber );
+        forReworkNumber = findViewById( R.id.forReworkNumber );
+
+//        TextView[] outputNumbers = {badNumber, goodNumber, skipNumber, forReworkNumber};
+
         mainWindowText.setOnTouchListener(new OnSwipeTouchListener(this) {
             @Override
             public void onSwipeRight() {
-                Toast.makeText(MainActivity.this, "Свайп вправо", Toast.LENGTH_SHORT).show();
+                increaseOutputNumber(goodNumber);
             }
 
             @Override
             public void onSwipeLeft() {
-                Toast.makeText(MainActivity.this, "Свайп влево", Toast.LENGTH_SHORT).show();
+                increaseOutputNumber(badNumber);
             }
 
             @Override
             public void onSwipeTop() {
-                Toast.makeText(MainActivity.this, "Свайп вверх", Toast.LENGTH_SHORT).show();
+                increaseOutputNumber(skipNumber);
             }
 
             @Override
             public void onSwipeBottom() {
-                Toast.makeText(MainActivity.this, "Свайп вниз", Toast.LENGTH_SHORT).show();
+                increaseOutputNumber(forReworkNumber);
             }
         });
     }
+
+    public void increaseOutputNumber(TextView number) {
+        int currentNumber = Integer.parseInt(number.getText().toString());
+        currentNumber++;
+        number.setText(String.valueOf(currentNumber));
+    }
+
 }
